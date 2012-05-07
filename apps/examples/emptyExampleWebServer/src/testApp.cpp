00db59c260c21dc88dc211bcdeb4896861a03c42
cheetahray <ray608@gmail.com>
2012-05-07 14:47:19 +0800

    DoReMi

---
+++ apps/examples/emptyExampleWebServer/src/testApp.cpp
@@ -0,0 +1,96 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	server.start("httpdocs");
+	server.addHandler(this, "/actions");
+	verdana30.loadFont("verdana.ttf", 12, true, true);
+	verdana30.setLineHeight(34.0f);
+	verdana30.setLetterSpacing(1.035);
+}
+
+
+void testApp::httpGet(string url) {
+	colorString = getRequestParameter("str");
+}
+
+void testApp::httpPost(string url, char *data, int dataLength) {
+	if(result.empty() == true)
+	{
+		colorString.assign(data+strlen("str="));
+		ofStringReplace(colorString, "%20", " %");
+		result = ofSplitString(colorString,"%");
+		result.erase (result.begin()+(result.size()-1));
+		result.erase (result.begin()+(result.size()-1));
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	if(result.empty() == false)
+	{
+		if(serial.setup("\\\\.\\COM17",9600))
+		{
+			vector<string>::iterator it;
+			string delayy = result.front();
+			int raydelay = ofToInt(delayy);
+			for ( it=result.begin() + 1 ; it < result.end(); it++ )
+			{
+				string rayray;
+				rayray.append(delayy).append(*it);
+				int raylen = strlen(rayray.data());
+				serial.writeBytes( (unsigned char*)rayray.data(), raylen );
+				ofSleepMillis(raydelay + (raylen << 1) );
+			}
+			result.clear();
+			ofStringReplace(colorString, "%", "\n");
+			size_t found = colorString.find_first_of("3B");
+			if (found!=string::npos)
+			{
+				colorString = colorString.substr(0,found);
+			}
+			serial.close();
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	verdana30.drawString(colorString, 30, 30 );
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
